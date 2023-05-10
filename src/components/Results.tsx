d706274db0a9f262ae57c009424c4c0c0d632105
ndgdl <nicolas.dgdl@inthememory.com>
2023-05-10 08:43:23 +0200

    feat: improve results comp (same results for all types)

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -2,6 +2,8 @@ import { type Run } from "~/constants/runConstants";
 import { type SpeedType } from "~/constants/speedConstants";
 import { HOUR } from "~/constants/timeConstants";
 import computeTotalTime from "~/helpers/computeTotalTime";
+import { toSpeed } from "~/helpers/convert";
+import { formatTwoDigitNumber } from "~/helpers/formatNumberString";
 
 interface ResultsProps {
   run: Run;
@@ -11,14 +13,20 @@ interface ResultsProps {
 
 function Results({ run, speedType, speed}: ResultsProps) {
   const { label, distance, distanceLabel} = run;
-  const runDescription = distanceLabel ? `the ${label} (${distanceLabel})` : `a ${label}`
-  const runTime = speedType === 'pace' ? speed * distance : (distance / speed) * HOUR
-
+  const runSpeed = speedType === 'speed' ? speed : toSpeed(speed);
+  const runTime = (distance / runSpeed) * HOUR;
+  
   const { hours, minutes, seconds } = computeTotalTime(runTime);
+  
+  const hoursInString = formatTwoDigitNumber(hours.toString());
+  const minutesInString = formatTwoDigitNumber(minutes.toString());
+  const secondsInString = formatTwoDigitNumber(seconds.toString());
+  
+  const runDescription = distanceLabel ? `the ${label} (${distanceLabel})` : `a ${label}`
 
   return (
     <div className="text-xl text-white">
-    {`You would run ${runDescription} in ${hours}:${minutes}:${seconds}`}
+    {`You would run ${runDescription} in ${hoursInString}:${minutesInString}:${secondsInString}`}
     </div>
   )
 }
