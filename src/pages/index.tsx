04958d1857d042970d6932ddc73a982ff7ad25b3
ndgdl <nicolas.dgdl@inthememory.com>
2023-05-10 11:22:12 +0200

    chore: index cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,16 +4,20 @@ import { useState } from "react";
 import Results from "~/components/Results";
 import SpeedInput from "~/components/SpeedInput";
 import ToggleRun from "~/components/ToggleRun";
+import { DEFAULT_RUN } from "~/constants/runConstants";
+import {
+  DEFAULT_PACE,
+  DEFAULT_SPEED,
+  type SpeedType,
+} from "~/constants/speedConstants";
+import { toPace, toSpeed } from "~/helpers/convert";
 import { paceToString } from "~/helpers/convertToString";
 import { stringToPace } from "~/helpers/parseString";
-import { DEFAULT_PACE, DEFAULT_SPEED, type SpeedType } from "~/constants/speedConstants";
-import { DEFAULT_RUN } from "~/constants/runConstants";
-import { toPace, toSpeed } from "~/helpers/convert";
 
 const Home: NextPage = () => {
   const [pace, setPace] = useState(DEFAULT_PACE);
   const [speed, setSpeed] = useState(DEFAULT_SPEED);
-  const [active, setActive] = useState<SpeedType>('pace');
+  const [active, setActive] = useState<SpeedType>("pace");
 
   const [run, setRun] = useState(DEFAULT_RUN);
 
@@ -32,50 +36,49 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Time my Run
           </h1>
-          <ToggleRun activeRun={run} setDistance={setRun}/>
-            <div className="grid grid-cols-1 space-y-2">
-              <SpeedInput
-                label="min/km"
-                speedType="pace"
-                speed={paceToString(pace)}
-                onChange={(event) => {
-                    const paceString = event.currentTarget.value;
-                    const pace = stringToPace(paceString);
-                    console.log(event.currentTarget.value)
-                    setPace(pace);
-                    console.log('pace:', pace);
+          <ToggleRun activeRun={run} setDistance={setRun} />
+          <div className="grid grid-cols-1 space-y-2">
+            <SpeedInput
+              label="min/km"
+              speedType="pace"
+              speed={paceToString(pace)}
+              onChange={(event) => {
+                const paceString = event.currentTarget.value;
+                const pace = stringToPace(paceString);
 
-                    if (pace > 0) {
-                      const speed = toSpeed(pace);
-                      console.log('speed:', speed);
-                      setSpeed(speed);
-                    }
-                  }
-                }
-                disabled={active !== 'pace'}
-                onClick={() => setActive('pace')}
-              /> 
-              <SpeedInput
-                label="km/h"
-                speed={speed}
-                speedType="speed"
-                onChange={(event) => {
-                    
-                    const speed = Number(event.currentTarget.value);
-                    setSpeed(speed);
+                setPace(pace);
 
-                    if  (speed > 0) {
-                      const pace = toPace(speed);
-                      setPace(pace);
-                    }
-                  }
+                if (pace > 0) {
+                  const speed = toSpeed(pace);
+                  setSpeed(speed);
                 }
-                disabled={active !== 'speed'}
-                onClick={() => setActive('speed')}
-              />
-            </div>
+              }}
+              disabled={active !== "pace"}
+              onClick={() => setActive("pace")}
+            />
+            <SpeedInput
+              label="km/h"
+              speed={speed}
+              speedType="speed"
+              onChange={(event) => {
+                const speed = Number(event.currentTarget.value);
+                setSpeed(speed);
+
+                if (speed > 0) {
+                  const pace = toPace(speed);
+                  setPace(pace);
+                }
+              }}
+              disabled={active !== "speed"}
+              onClick={() => setActive("speed")}
+            />
           </div>
-          <Results run={run} speedType={active} speed={active === 'pace' ? pace : speed}/>
+        </div>
+        <Results
+          run={run}
+          speedType={active}
+          speed={active === "pace" ? pace : speed}
+        />
       </main>
     </>
   );
